f30af2136bf41868c5ef002293bf07fd3e22e5bc
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2020-02-04 08:45:15 +0530

    controller updated with rewuest param

--- src/main/java/com/spring/allinone/controller/AllInOneController.java
+++ src/main/java/com/spring/allinone/controller/AllInOneController.java
@@ -1,7 +1,9 @@
 package com.spring.allinone.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,9 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AllInOneController {
 	
-	@GetMapping("/{name}")
+	@GetMapping("/sayHello/{name}")
 	public String sayHello(@PathVariable("name") String name) {
 		return "Hello : "+name;
+	}
+
+	@GetMapping(value="/sayHi",produces = MediaType.TEXT_PLAIN_VALUE)
+	public String sayHi(@RequestParam("name") String name) {
+		return "Hi :"+name;
 	}
 
 }
