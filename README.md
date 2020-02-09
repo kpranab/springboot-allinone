09cb346b5b861424cb76ad403274c15062ea9c2d
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2020-02-09 12:21:20 +0530

    redmi file updated with test url and properties file value read using @Value and profile file added for development

--- README.md
+++ README.md
@@ -12,3 +12,9 @@ build the project mvn clean install
 create mainfest.yml file and add the required configuration 
 
 cf push  -->Note : Make sure your manifest.yml is in same directory
+
+
+Test URLs:
+
+http://localhost:9090/sayHello/Pranab
+http://localhost:9090/sayHi?name=Pranab
--- src/main/java/com/spring/allinone/controller/AllInOneController.java
+++ src/main/java/com/spring/allinone/controller/AllInOneController.java
@@ -1,5 +1,8 @@
 package com.spring.allinone.controller;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,14 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AllInOneController {
 	
+	@Value("${message.hello}")
+	private String hello;
+
 	@GetMapping("/sayHello/{name}")
 	public String sayHello(@PathVariable("name") String name) {
-		return "Hello : "+name;
+		return "Hello : " +name;
 	}
 
 	@GetMapping(value="/sayHi",produces = MediaType.TEXT_PLAIN_VALUE)
 	public String sayHi(@RequestParam("name") String name) {
 		return "Hi :"+name;
+	}
+
+	@PostConstruct
+	private void postConstruct() {
+		System.out.println(hello);
 	}
 
 }
